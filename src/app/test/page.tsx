696346f3b2de34f1a2bae3d96d52de980564c80b
Lee-h-0 <alohaquarium@gmail.com>
2025-09-04 13:51:27 +0900

    chore: tailwind 커스텀 컬러 추가

---
+++ src/app/test/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="">
+      <div className=" bg-primary">primary</div>
+      <div className=" bg-secondary">secondary</div>
+      <div className=" bg-tertiary">tertiary</div>
+      <div className=" bg-quaternary">quaternary</div>
+      <div className=" bg-quinary">quinary</div>
+      <div className=" bg-senary">senary</div>
+      <div className=" bg-septenary">septenary</div>
+    </div>
+  );
+}
